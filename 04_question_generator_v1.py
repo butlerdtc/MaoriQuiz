130525f7f24463734db841789902f8ca3907595f
butlerdtc <butlerr2@middleton.school.nz>
2023-05-16 21:45:06 +1200

    Update

--- 04_question_generator_v1.py
+++ 04_question_generator_v1.py
@@ -1,50 +1,31 @@
 """ V1 of the random question generator """
+# This imports random for use in program
+import random
 
+# The set of questions for difficulty level 1
+questions_1 = [["What is tahi in english? ", "1"],
+               ["What is rua in english? ", "2"],
+               ["What is toru in english? ", "3"],
+               ["What is wha in english? ", "4"],
+               ["What is rima in english? ", "5"],
+               ["What is ono in english? ", "6"],
+               ["What is whitu in english? ", "7"],
+               ["What is waru in english? ", "8"],
+               ["What is iwa in english? ", "9"],
+               ["What is tekau in english? ", "10"]]
 
-# Function to ask user for a difficulty level
-def difficulty_levels():
-    # Statement to explain differences in difficulty levels
-    print()
-    print("This is a quiz about numbers in Maori. "
-          "This quiz has three different levels of difficulty.\n"
-          "Level 1 is the easiest difficulty\n"
-          "Level 2 is medium difficulty\n"
-          "Level 3 is the hardest difficulty\n"
-          "You were recommended a difficulty earlier, but you are free to "
-          "choose any level."
-          "\n"
-          "You can also come back for the other difficulty levels.")
-    print()
+# This randomly shuffles the order of the list
+random.shuffle(questions_1)
 
-    # While loop to check if user input is valid/invalid then run accordingly
-    while True:
-        # Asks for user input
-        level = input("Please choose a difficulty, enter 1, 2, or 3: ")
-        # Checks if input is valid (1, 2, or 3) then ends the loop
-        if level == "1" or level == "2" or level == "3":
-            # Prints user input
-            print(f"You entered {level}")
-            # If level is 1 it will set chosen_level to 1
-            if level == "1":
-                chosen_level = 1
-            # If level is 2 it will set chosen_level to 2
-            elif level == "2":
-                chosen_level = 2
-            # If level is anything else (only possible remaining value is 3) it
-            # will set chosen_value to 3
-            else:
-                chosen_level = 3
-            # Prints result of user input
-            print(f"You have chosen a difficulty level of {chosen_level}")
-            return chosen_level
-        # If invalid the loop repeats
-        else:
-            print("Invalid input, please choose a valid difficulty level")
-
-
-# Main routine
-difficulty_levels()
-
-list_1 = []
-list_2 = []
-list_3 = []
+# This loop asks the questions from the list and will ask for input and then
+# check if the answer is correct. It will ask  each question once.
+for i in questions_1:
+    answer = input("Please enter the answer to the question {}: ".format(i[0]))
+    if answer == i[1]:
+        # If answer is correct this prints correct
+        print("correct")
+    else:
+        # If answer is incorrect this prints incorrect
+        print("incorrect")
+# Test Statement
+print("End")
--- test.py
+++ test.py
@@ -1,4 +1,9 @@
+import random
+
 print("Assessment")
 # Testing my knowledge on the '.isdigit()' method
 hi = "9090"
 print(hi.isdigit())
+
+mon = ["1", "2", "3"]
+random.shuffle(mon)
