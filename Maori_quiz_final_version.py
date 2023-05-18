1a3dc1161589529e8031cb0221c01652f89e2348
butlerdtc <butlerr2@middleton.school.nz>
2023-05-19 10:03:06 +1200

    Update Maori_quiz_final_version.py

--- Maori_quiz_final_version.py
+++ Maori_quiz_final_version.py
@@ -12,6 +12,7 @@ def formatter(symbol, text):
     sides = symbol * 3
     formatted_text = f"{sides} {text} {sides}"
     top_bottom = symbol * len(formatted_text)
+    # Returns the formatted statement
     return f"{top_bottom}\n{formatted_text}\n{top_bottom}"
 
 
